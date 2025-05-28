408dfbae9accb4c04d3724844d8d4fb547df02bb
zhuhaow <zhuhaow@gmail.com>
2025-05-28 13:27:15 +0800

    Update generated code

--- src/parser.c
+++ src/parser.c
@@ -29823,8 +29823,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_rune(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 0,
+      .minor_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
